dec7e5c59f3e90f14baffed23f04fbb6bc848873
Andrew Wen <Wen.Andrew@mayo.edu>
2023-06-02 16:35:04 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+with open("requirements.txt") as f:
+    requirements = f.readlines()
+
+setup(
+    name="ohnlp-backbone-xlang-python",
+    version="1.0.0",
+    description="Python support for OHNLP Toolkit Backbone Components",
+    author="Andrew Wen",
+    author_email="contact@ohnlp.org",
+    packages=['ohnlp'],
+    install_requires=requirements
+)
