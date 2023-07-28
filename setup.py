4fcd3e0314622049eaae52e846e5ce3a651dbbd6
Andrew Wen <Andrew.Wen@uth.tmc.edu>
2023-07-28 17:23:59 -0500

    Increment to v1.0.0 for fully tested release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ohnlptk-xlang-python",
-    version="0.0.3",
+    version="1.0.0",
     description="Python support for OHNLP Toolkit Components",
     author="Andrew Wen",
     author_email="contact@ohnlp.org",
